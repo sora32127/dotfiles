d8b0cb90e45efda7c311b524fc5cb88519fbe0b8
contradiction29 <sora32127@gmail.com>
2023-10-03 21:29:43 +0900

    Add file for setup script for Ubuntu

---
+++ setup_script_for_ubuntu.sh
@@ -0,0 +1,35 @@
+# This is setup script for Ubuntu
+
+# Default shell : zsh
+
+# Install zsh
+sudo apt install zsh
+
+# set default shell
+chsh -s $(which zsh)
+touch ~/.zshrc
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.zprofile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+source ~/.zshrc
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install antigen
+brew install antigen
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' ~/.zshrc
+
+# Install zsh-autosuggestions
+brew install zsh-autosuggestions
+echo source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh >> ~/.zshrc
+
+# Install zsh-autocomplete
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+echo source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh >> ~/.zshrc
